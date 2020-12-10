36e848861d0d2e602dee7063f2ae48f38727c38e
Gabe Pressman <gpressman@ethermed.ai>
2020-12-09 21:40:40 -0700

    add routing to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Search } from "./components/Search";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Repository } from "./components/Repository";
 
 function App() {
-  return <Search />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/repositories/:owner/:repo" component={Repository} />
+        <Route path="/" component={Search} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
